9b87988b284b1c92e6d0dd073f32ba1756e99a71
MatteoLoddo <loddomatteo96@gmail.com>
2022-07-26 16:40:05 +0200

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,49 @@
+@php
+
+$navbarItems = [
+    [
+        'title' => 'characters',
+        'link' => route('characters'),
+    ],
+    [
+        'title' => 'characters',
+        'link' => route('characters'),
+    ],
+    [
+        'title' => 'characters',
+        'link' => route('characters'),
+    ],
+    [
+        'title' => 'characters',
+        'link' => route('characters'),
+    ],
+    [
+        'title' => 'characters',
+        'link' => route('characters'),
+    ],
+    [
+        'title' => 'characters',
+        'link' => route('characters'),
+    ],
+    [
+        'title' => 'characters',
+        'link' => route('characters'),
+    ],
+];
+@endphp
+
+
+<header class="container m-auto">
+    <div class="d-flex justify-content-between">
+        <div class="img-container">
+            <img src="{{ asset('img/dc-logo.png') }}" alt="">
+        </div>
+        <div class="d-flex align-items-center">
+            <ul class="list-unstyled d-flex justify-content-end ">
+                @foreach ($navbarItems as $item)
+                    <li class="d-flex align-self-center"><a class="fw-bold text-decoration-none p-2 align-self-center" href="{{ $item['link'] }}">{{ strtoupper($item['title']) }}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</header>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title_page' , 'Dc-Comics')
+
+
+@section('content')
+<div class="jumbotron" >
+
+</div>
+
+
+@endsection
